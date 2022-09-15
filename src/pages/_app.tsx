36fe9b155b79da431fc18d89fe616afa18e38354
Ebrahim <ebrahim.baghbani73@gmail.com>
2022-09-15 13:47:46 +0430

    feat: add i18n to app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,13 @@
 import "@/styles/css/tailwind.css";
 import "@/styles/scss/main.scss";
 import type { ReactElement, ReactNode } from "react";
+import { Toaster } from "react-hot-toast";
 import type { NextPage } from "next";
 import type { AppProps } from "next/app";
 import { ReactQueryDevtools } from "react-query/devtools";
 import { QueryClientProvider, QueryClient } from "react-query";
+import { appWithTranslation } from "next-i18next";
+import nextI18nConfig from "../../next-i18next.config";
 
 export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
 	getLayout?: (page: ReactElement) => ReactNode;
@@ -16,13 +19,15 @@ type AppPropsWithLayout = AppProps & {
 
 const queryClient = new QueryClient();
 
-export default function MyApp({ Component, pageProps }: AppPropsWithLayout) {
+function MyApp({ Component, pageProps }: AppPropsWithLayout) {
 	const getLayout = Component.getLayout ?? ((page) => page);
 
 	return (
 		<QueryClientProvider client={queryClient}>
 			{getLayout(<Component {...pageProps} />)}
 			<ReactQueryDevtools />
+			<Toaster />
 		</QueryClientProvider>
 	);
 }
+export default appWithTranslation(MyApp, nextI18nConfig);
