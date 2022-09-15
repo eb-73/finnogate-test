fa7284e5d165f94f8194fffb492b500900a4ad15
Ebrahim <ebrahim.baghbani73@gmail.com>
2022-09-15 13:48:44 +0430

    feat: create instansce from axios

--- src/services/utils/axios.ts
+++ src/services/utils/axios.ts
@@ -1,7 +1,7 @@
-import { mainUrl } from "@/healpers/env-variables";
+import { loginUrl } from "@/helpers/env-variables";
 import _axios from "axios";
 
 const axios = _axios.create({
-	baseURL: mainUrl,
+	baseURL: loginUrl,
 });
 export default axios;
